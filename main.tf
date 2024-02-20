04e4976e3374f919cfa5653c918baa01f55154fd
Rushikesh Deore <deore.r@northeastern.edu>
2024-02-20 14:34:52 -0500

    Correcting fixes about hardcoded values which was suggested in grading

--- main.tf
+++ main.tf
@@ -1,38 +1,38 @@
 provider "google" {
   #credentials = file("<path-to-your-service-account-key.json>")
-  project     = "csye6225-ns-cloud"
-  region      = "us-east1"
+  project     = var.project_id
+  region      = var.region
 }
 
 # Create VPC
-resource "google_compute_network" "my_vpc" {
-  name                    = "my-vpc"
-  auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
+resource "google_compute_network" "mainvpc" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = var.auto_create_subnetworks
+  routing_mode            = var.routing_mode
  #delete_default_routes_on_create = true
 }
 
 # Create Subnets
-resource "google_compute_subnetwork" "webapp" {
-  name          = "webapp"
-  ip_cidr_range = "10.0.0.0/24"
-  region        = "us-east1"
-  network       = google_compute_network.my_vpc.name
+resource "google_compute_subnetwork" "subnet_1" {
+  name          = var.subnetwork_name_1
+  ip_cidr_range = var.subnetwork_ip_cidr_range_1
+  region        = var.region
+  network       = google_compute_network.mainvpc.name
 }
 
-resource "google_compute_subnetwork" "db" {
-  name          = "db"
-  ip_cidr_range = "10.0.1.0/24"
-  region 	= "us-east1"
-  network       = google_compute_network.my_vpc.name
+resource "google_compute_subnetwork" "subnet_2" {
+  name          = var.subnetwork_name_2
+  ip_cidr_range = var.subnetwork_ip_cidr_range_2
+  region 	= var.region
+  network       = google_compute_network.mainvpc.name
 }
 
 # Add Route for webapp subnet
-resource "google_compute_route" "webapp_route" {
-  name         = "webapp-route"
-  network      = google_compute_network.my_vpc.self_link
-  dest_range   = "0.0.0.0/0"
-  next_hop_gateway = "default-internet-gateway"
+resource "google_compute_route" "subnet_route" {
+  name         = var.route_name
+  network      = google_compute_network.mainvpc.self_link
+  dest_range   = var.dest_range
+  next_hop_gateway = var.next_hop_gateway
 
 }
 
