e938030593e0fffd491c17ae75dc4cd17708b5bc
Rushikesh Deore <deore.r@northeastern.edu>
2024-02-21 20:45:30 -0500

    Firewall rules and variable rules updated

--- variables.tf
+++ variables.tf
@@ -67,3 +67,28 @@ variable "next_hop_gateway" {
   description = "The next hop gateway for the route"
   type        = string
 }
+
+variable "custom_image" {
+  description = "The image created through packer build"
+  type        = string
+}
+
+variable "application_port" {
+  description = "The port application listens to"
+  type        = string
+}
+
+variable "firewall_name" {
+  description = "The name of the firewall being created"
+  type        = string
+}
+
+variable "firewall_protocol" {
+  description = "The protocol new firewall will follow"
+  type        = string
+}
+
+variable "compute_instance" {
+  description = "The compute engine instanc asked to be created"
+  type        = string
+}
